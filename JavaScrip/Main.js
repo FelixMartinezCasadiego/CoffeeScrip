8b341758915309477f844f0acb9f42a2c5f8103f
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-17 21:42:25 -0300

    Primer commit

---
+++ JavaScrip/Main.js
@@ -0,0 +1,5 @@
+// Prompt de Saludo
+
+var IngresoCliente = prompt('Ingrese su Nombre:');
+console.log('Mi nombre es: ' + IngresoCliente);
+alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");
