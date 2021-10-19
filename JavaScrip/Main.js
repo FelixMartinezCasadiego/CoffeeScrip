7806d3711eae9a88544df9028eb2328cf1f9488f
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-18 22:00:45 -0300

    Agregado de Js de primera entrega

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -3,3 +3,28 @@
 var IngresoCliente = prompt('Ingrese su Nombre:');
 console.log('Mi nombre es: ' + IngresoCliente);
 alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");
+
+// Temas para primera entrega (Pedir texto)
+
+let CantidadCafes = prompt ('Cuantos cafes deseas:');
+console.log('La cantidad solicitada es ' + CantidadCafes);
+
+alert ('Gracias ' + IngresoCliente + ' por la compra de ' + CantidadCafes + ' café');
+
+// Temas para primera entrega (Pedir número)
+
+let CantidadPedida1 = 1;
+let CantidadPedida2 = 2;
+let CantidadPedida3 = 3;
+
+    // Suma (1 + 2 = 3)
+    let ResultadoSuma = CantidadPedida1 + CantidadPedida2;
+    console.log('El resultado de la suma es: ' + ResultadoSuma);
+    // Resta (2 - 1 = 1)
+    let ResultadoResta = CantidadPedida2 - CantidadPedida1;
+    console.log('El resultado de la resta es: ' + ResultadoResta)
+
+
+
+
+
