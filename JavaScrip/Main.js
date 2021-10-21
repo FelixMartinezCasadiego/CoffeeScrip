f8b2a574bb5814a66c3aeeb71d62938cb3cb5415
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-20 21:49:35 -0300

    Entrega de 2do trabajo

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,5 +1,33 @@
-// Prompt de Saludo
+// Segunda Entrega
 
-var IngresoCliente = prompt('Ingrese su Nombre:');
-console.log('Mi nombre es: ' + IngresoCliente);
-alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");
+// Pedir número mediante prompt y si es mayor a 1000 mostrar un alert
+
+var CantidadPedido = prompt('Cuantas bebidas deseas?');
+console.log('La cantidad de bebida que pediste es: ' + CantidadPedido);
+
+if((CantidadPedido >= 1000)){
+    alert('No crees que ' + CantidadPedido + ' es demasiado, take it easy');
+}else{
+    console.log('Gracias por la compra de ' + CantidadPedido + ' cafes');
+};
+
+// Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola
+
+var NombreCliente = prompt('Cual es el Nombre de la persona que recibe la bebida?');
+console.log('El nombre del cliente es: ' + NombreCliente);
+
+if((NombreCliente == 'Hola') || (NombreCliente == 'hola')){
+     alert('Coloca un nombre correcto, estas salundo');
+}else{
+     console.log('Gracias por realizar tu compra ' + NombreCliente);
+};
+
+// Pedir un número por prompt y evaluar si está entre 10 y 50. En caso positivo mostrar un alert
+
+var TiempoEspera = prompt('Cuantos minutos duraste esperando tu bebida?');
+
+if(( TiempoEspera >= 10 && TiempoEspera <= 50)){
+    alert('Disculpa por tanto tiempo de demora');
+}else{
+    console.log('Los minutos de espera fue: ' + TiempoEspera);
+};
