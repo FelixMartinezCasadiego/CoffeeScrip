e47c273050682b2c4252a0f8bb364170ac42313d
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-22 23:21:00 -0300

    Creación de rama TerceraEntrega

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,5 +1,27 @@
-// Prompt de Saludo
+// Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
+/*
+var CantidadBebidas = prompt('Cuantas bebidas deseas?');
+console.log('La cantidad deseada es: ' + CantidadBebidas);
+for (let i= CantidadBebidas; i<=10; i++){
+    console.log('El valor es: ' + i);
+};*/
 
-var IngresoCliente = prompt('Ingrese su Nombre:');
-console.log('Mi nombre es: ' + IngresoCliente);
-alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");
+// Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”
+
+/*
+var IngreseMonto = prompt('Cuanto es el monto a pagar : ');
+console.log('El monto a pagar es: ' + IngreseMonto);
+
+
+while ( IngreseMonto != 'ESC') {
+    alert('El monto ingresado es ' + IngreseMonto);
+
+    IngreseMonto = prompt ('Ingrese otro monto');
+}*/
+
+// - Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada
+
+Var NumeroPedido = prompt('Ingrese el número favorito:');
+
+
+
