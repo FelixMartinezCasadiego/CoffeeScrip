b599ffb8878fa889db0721e3ea3bc55af1ff1664
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-24 22:14:54 -0300

    Se realiza tarea solicitada

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,14 +1,15 @@
 // Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
-/*
+
 var CantidadBebidas = prompt('Cuantas bebidas deseas?');
 console.log('La cantidad deseada es: ' + CantidadBebidas);
+
 for (let i= CantidadBebidas; i<=10; i++){
     console.log('El valor es: ' + i);
-};*/
+};
 
 // Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”
 
-/*
+
 var IngreseMonto = prompt('Cuanto es el monto a pagar : ');
 console.log('El monto a pagar es: ' + IngreseMonto);
 
@@ -17,11 +18,6 @@ while ( IngreseMonto != 'ESC') {
     alert('El monto ingresado es ' + IngreseMonto);
 
     IngreseMonto = prompt ('Ingrese otro monto');
-}*/
-
-// - Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada
-
-var NumeroPedido = prompt('Ingrese el número favorito:');
-
+}
 
 
