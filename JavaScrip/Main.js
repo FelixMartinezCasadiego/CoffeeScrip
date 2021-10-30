9567ef8b78bada21aa0163d0e43f81cf7a437457
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-30 18:58:16 -0300

    creación de rama

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,23 +1,6 @@
-// Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
+// Saludo
 
-var CantidadBebidas = prompt('Cuantas bebidas deseas?');
-console.log('La cantidad deseada es: ' + CantidadBebidas);
-
-for (let i= CantidadBebidas; i<=10; i++){
-    console.log('El valor es: ' + i);
-};
-
-// Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”
-
-
-var IngreseMonto = prompt('Cuanto es el monto a pagar : ');
-console.log('El monto a pagar es: ' + IngreseMonto);
-
-
-while ( IngreseMonto != 'ESC') {
-    alert('El monto ingresado es ' + IngreseMonto);
-
-    IngreseMonto = prompt ('Ingrese otro monto');
+function Saludo(){
+    let
 }
-
 
