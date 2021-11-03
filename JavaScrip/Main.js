f414a8cd13bb50a43ec4673a96f72eb4caa836d1
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-11-02 22:48:10 -0300

    Creación de rama para entrega

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -4,3 +4,15 @@
 console.log('Mi nombre es: ' + IngresoCliente);
 alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");*/
 
+// Creación de objeto
+
+function Cliente1(var1, var2, var3){
+    this.cliente = var1;
+    this.cafe = var2;
+    this.despedida = var3;
+}; 
+
+const Persona1 = new Cliente1(prompt('Cual es tu nombre? '), prompt('cuantos cafes quieres? '), alert('Estará en 2min ! Gracias por tu compra !'));
+
+console.log(Persona1);
+
