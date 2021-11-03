9047a8360cf8dbbcb2fd8b3346be75e135cb5c75
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-11-02 23:18:52 -0300

    se agrega tarea

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -6,13 +6,34 @@ alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");*/
 
 // Creación de objeto
 
-function Cliente1(var1, var2, var3){
+function Cliente1(var1, var2, var3){ 
+    
+
     this.cliente = var1;
     this.cafe = var2;
     this.despedida = var3;
+
 }; 
 
 const Persona1 = new Cliente1(prompt('Cual es tu nombre? '), prompt('cuantos cafes quieres? '), alert('Estará en 2min ! Gracias por tu compra !'));
 
 console.log(Persona1);
 
+// Metodo
+
+class Vaso {
+    constructor (nombre, precio){
+        this.nombre = nombre.toUpperCase();
+        this.precio = precio;
+    }
+
+    iva() {
+        this.precio = this.precio * 1.21;
+    }
+};
+
+const venta1 = new Vaso(prompt('Que nombre quieres en el vaso?'), 2 );
+console.log(venta1);
+
+venta1.iva();
+console.log();
