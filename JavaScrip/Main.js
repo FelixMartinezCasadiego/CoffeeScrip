0b5796007c091b85ea3a94ab0384bb5e23846e16
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-11-06 17:44:40 -0300

    agregado de información al master

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,6 +1,37 @@
-// Prompt de Saludo
+// Creación de objeto
 
-/*var IngresoCliente = prompt('Ingrese su Nombre:');
-console.log('Mi nombre es: ' + IngresoCliente);
-alert('Welcome to CoffeeScrip' + " " + IngresoCliente + " !");*/
+function Cliente1(var1, var2){ 
+    
+
+    this.cliente = var1;
+    this.bienvenida = var2;
+
+}; 
+
+const Persona1 = new Cliente1(prompt('Ingrese su Nombre: '), alert('Welcome to CoffeeScrip!') );
+
+console.log(Persona1);
+
+var cantidadCafe = prompt('cuantos cafes quieres? ')
+alert('Estará en 2min ! Gracias por tu compra !')
+
+// Metodo
+
+class Vaso {
+    constructor (nombre, precio){
+        this.nombre = nombre.toUpperCase();
+        this.precio = precio;
+    }
+
+    iva() {
+        this.precio = this.precio * 1.21;
+    }
+};
+
+const venta1 = new Vaso(prompt('Que nombre quieres en el vaso?'), 2 );
+console.log(venta1);
+
+venta1.iva();
+console.log(venta1);
+
 
