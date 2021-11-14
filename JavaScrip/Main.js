d7eea286f97cf9d2609c3ee7191a64425ad04bdd
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-11-13 23:50:57 -0300

    Se agrega rama OctavaEntrega

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -1,37 +1,108 @@
-// Creación de objeto
+/* 
 
-function Cliente1(var1, var2){ 
-    
+Arrays -> Stock // carrito (producto elegido por el usuario)
+Objetos -> Instanciar los distintos productos que tiene la tienda
+Clases -> moldeadores de los objetos
+Funciones -> Agregar items al carrito
+Ciclos -> se podrá sumar items al carrito
 
-    this.cliente = var1;
-    this.bienvenida = var2;
+*/
 
-}; 
+// Class moldeadora de objetos
 
-const Persona1 = new Cliente1(prompt('Ingrese su Nombre: '), alert('Welcome to CoffeeScrip!') );
-
-console.log(Persona1);
-
-var cantidadCafe = prompt('cuantos cafes quieres? ')
-alert('Estará en 2min ! Gracias por tu compra !')
-
-// Metodo
-
-class Vaso {
-    constructor (nombre, precio){
-        this.nombre = nombre.toUpperCase();
+class producto{
+    constructor (id, nombre, precio, stock){
+        this.id = id;
+        this.nombre = nombre;
         this.precio = precio;
+        this.stock = stock;
     }
 
-    iva() {
-        this.precio = this.precio * 1.21;
+    // metodo del objeto para restar stock
+
+    restarstock(){
+        this.stock = this.stock - 1;
+
+        console.log(`El stock de ${this.nombre} ha sido actualizado`);
     }
+}
+
+// Objetos instanciar manualmente
+
+const producto0 = new producto (0, 'cafe', 350, 100);
+const producto1 = new producto (1, 'capuchino', 88, 20);
+const producto2 = new producto (2, 'cafe con leche', 85, 50);
+const producto3 = new producto (3, 'cafe latte', 200, 90);
+
+let arrayProductos = [producto0, producto1, producto2, producto3];
+
+console.log(arrayProductos);
+
+console.table(arrayProductos);
+
+let carrito = [];
+
+let productosOfrecidos = 'Tenemos para ofrecerle: ';
+
+// Muestra via prompt con productos
+
+function agregarCarrito(){
+    // Recorremos arrayProductos y por cada elemento que tenga se suma el item
+
+    for (item of arrayProductos){
+
+        productosOfrecidos += ` \n ${item.id} - ${item.nombre} a $ ${item.precio} la unidad `
+    }
+
+productosOfrecidos += '\n Ingrese el número de item que desea agregar al carrito. Para finalizar ingrese 99.'
+
+let respuestaUsuario = parseInt(prompt(productosOfrecidos))
+
+while(isNaN(respuestaUsuario)){
+    alert('Por favor, ingrese solo números');
+    respuestaUsuario = parseInt(prompt(productosOfrecidos))
+}
+
+while(respuestaUsuario != 99){
+    switch(respuestaUsuario){
+
+        case 0:
+            carrito.push(arrayProductos[0])
+            alert(`Agregamos al carrito ${arrayProductos[0].nombre}`)
+            arrayProductos[0].restarstock()
+            break;
+
+        case 1:
+            carrito.push(arrayProductos[1])
+            alert(`Agregamos al carrito ${arrayProductos[1].nombre}`)
+            arrayProductos[1].restarstock()
+            break;
+
+        case 2:
+            carrito.push(arrayProductos[2])
+            alert(`Agregamos al carrito ${arrayProductos[2].nombre}`)
+            arrayProductos[2].restarstock()
+            break;
+
+        case 3:
+        carrito.push(arrayProductos[3])
+        alert(`Agregamos al carrito ${arrayProductos[3].nombre}`)
+        arrayProductos[3].restarstock()
+        break;
+
+        default: 
+        console.log('No tenemos el id elegido')
+        break;
+    }
+}
+
+respuestaUsuario = parseInt(prompt(productosOfrecidos))
+
 };
 
-const venta1 = new Vaso(prompt('Que nombre quieres en el vaso?'), 2 );
-console.log(venta1);
+agregarCarrito();
 
-venta1.iva();
-console.log(venta1);
+
+
 
 
