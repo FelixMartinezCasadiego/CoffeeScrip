bca425387fc911e9ea6000c10ba6efbb56f45947
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-11-19 23:43:50 -0300

    Se crea primera fase de carrito de compras

--- JavaScrip/Main.js
+++ JavaScrip/Main.js
@@ -25,7 +25,7 @@ addShoppingCarrito.forEach( (addToCarButton) => {
     addToCarButton.addEventListener('click', addToCarClicked);
 });
 
-const shoppingCarItemsContainer = document.querySelector('.shoppingCarItemsContainer');
+const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
 
 function addToCarClicked(event) {
     const button = event.target;
@@ -35,10 +35,92 @@ function addToCarClicked(event) {
     const itemPrice = item.querySelector('.item-Price').textContent;
     const itemImg = item.querySelector('.item-Img').src;
 
-    addItemToShoppingCar(itemTitle, itemPrice, itemImg);
+    addItemToShoppingCart(itemTitle, itemPrice, itemImg);
 }
 
-function addItemToShoppingCar(itemTitle, itemPrice, itemImg){
-    const shoppingCarRow = document.createElement('div');
-    const shoppingCartContent = 
-}
+function addItemToShoppingCart(itemTitle, itemPrice, itemImg){
+
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName('.shoppingCartItemTittle');
+
+    for(let i = 0; i < elementsTitle.length; i++){
+        if (elementsTitle[i].innerText === itemTitle){
+            let elementQuantity = elementsTitle[i].parentElement.parentElement.parentElement.
+            querySelector('.shoppingCartItemQuantity');
+            elementQuantity.value++;
+            $('.toast').toast('show');
+            return;
+        }
+    };
+
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+    <div class="row shoppingCartItem">
+
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImg} class="shopping-cart-image" width="100px">
+                <h6 class="shopping-cart-item-tittle shoppingCartItemTittle text-truncate ml-3 mb-0 ms-3">${itemTitle}</h6>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input type="number" class="shopping-cart-quantity-input shoppingCartItemQuantity" value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div> `;
+
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow.querySelector('.buttonDelete').addEventListener('click', removeShoppingCartItem);
+
+    shoppingCartRow.querySelector('.shoppingCartItemQuantity');
+    addEventListener('change', quantityChanged);
+
+    updateShoppingCartTotal();
+
+};
+
+function updateShoppingCartTotal(){
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+    shoppingCartItems.forEach(shoppingCartItem => {
+
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$', ''));
+
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+    });
+
+    shoppingCartTotal.innerHTML = `$ ${total}`;
+};
+
+function removeShoppingCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+};
+
+function quantityChanged(event) {
+    const input = event.target;
+    if (input.value <= 0){
+        input.value = 1;
+    };
+    updateShoppingCartTotal();
+};
